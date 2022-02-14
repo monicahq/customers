97cac0ce0dc2b3bf4a2bc9725a1af2e116e22470
Regis Freyd <djaiss@users.noreply.github.com>
2022-02-14 17:23:42 -0500

    wiip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,25 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('Dashboard') }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    OfficeLife requires a license key. This customer portal lets you purchase a license for your company, or renew a license.
-                </div>
-
-            </div>
+  <div class="py-12">
+    <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200">
+          <p class="mb-2">OfficeLife requires a license key. This customer portal lets you purchase a license for your company, or renew a license.</p>
+          <p>Your license won't automatically renew.</p>
         </div>
+
+        <!-- cta -->
+        <div class="text-center p-6">
+          <a href="/purchase" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+            {{ __('Purchase a license') }}
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,4 +12,5 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('purchase', [PurchaseController::class, 'index'])->name('purchase.index');
 });
