a486fc41badba5fb2f22e4a7903014207232c7d9
Regis Freyd <djaiss@users.noreply.github.com>
2022-03-01 22:18:31 -0500

    wip

--- app/Listeners/PaddleEventListener.php
+++ app/Listeners/PaddleEventListener.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use Illuminate\Support\Facades\Log;
 use App\Models\Plan;
 use App\Models\LicenceKey;
 use Laravel\Paddle\Events\WebhookReceived;
@@ -29,7 +30,11 @@ class PaddleEventListener
         if ($event->payload['alert_name'] === 'subscription_payment_succeeded') {
             $plan = Plan::where('plan_id_on_paddle', $event->payload['subscription_plan_id'])->first();
 
-            $licenceKey = LicenceKey::where('user_id', $event->payload['passthrough']['billable_id'])
+            $billableId = json_decode($event->payload['passthrough'], true);
+            $billableId = $billableId['billable_id'];
+            Log::info($billableId);
+
+            $licenceKey = LicenceKey::where('user_id', $billableId)
                 ->where('plan_id', $plan->id)
                 ->first();
 
