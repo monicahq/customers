d2002786dfa0761130d53cedc9079aeed831b849
Alexis Saettler <alexis@saettler.org>
2022-05-22 12:40:35 +0200

    remove laravel/breeze

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -30,9 +30,9 @@
                     width="150"
                 />
 
-                <inertia-link :href="'officelife'" class="mb-4 focus:shadow-outline-gray inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:border-gray-900 focus:outline-none active:bg-gray-900">
+                <Link :href="route('officelife.index')" class="mb-4 focus:shadow-outline-gray inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:border-gray-900 focus:outline-none active:bg-gray-900">
                   Manage your subscription
-                </inertia-link>
+                </Link>
 
               </div>
 
@@ -43,9 +43,9 @@
                     width="150"
                 />
 
-                <inertia-link :href="'monica'" class="mb-4 focus:shadow-outline-gray inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:border-gray-900 focus:outline-none active:bg-gray-900">
+                <Link :href="route('monica.index')" class="mb-4 focus:shadow-outline-gray inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:border-gray-900 focus:outline-none active:bg-gray-900">
                   Manage your subscription
-                </inertia-link>
+                </Link>
 
               </div>
             </div>
@@ -77,6 +77,7 @@
 
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/inertia-vue3';
 
 export default {
   components: {
