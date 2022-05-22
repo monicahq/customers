a55382f46ad854f46249c5ef97aecbc20dba9891
Alexis Saettler <alexis@saettler.org>
2022-05-22 09:51:33 +0200

    rename

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -16,7 +16,7 @@ defineProps({
     <div class="flex flex-col justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
         <nav v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             <Link v-if="$page.props.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
-                Dashboard
+                Home
             </Link>
 
             <template v-else>
