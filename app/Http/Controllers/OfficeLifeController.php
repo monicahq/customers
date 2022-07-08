a908bad4ce3ee1d2b01d8753d994b7155038a73d
Alexis Saettler <alexis@saettler.org>
2022-07-08 23:19:58 +0200

    feat: ability to switch from 1 plan to another (#40)

--- app/Http/Controllers/OfficeLifeController.php
+++ app/Http/Controllers/OfficeLifeController.php
@@ -5,21 +5,37 @@ namespace App\Http\Controllers;
 use App\Helpers\Products;
 use App\Http\Requests\OfficeLifePriceRequest;
 use App\Models\Plan;
+use App\Services\UpdateSubscription;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class OfficeLifeController extends Controller
 {
     public const PRODUCT = 'OfficeLife';
 
-    public function index(Request $request)
+    /**
+     * Display OfficeLife licences.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Inertia\Response
+     */
+    public function index(Request $request): Response
     {
+        $subscription = $request->user()->subscriptions()
+            ->active()
+            ->product(static::PRODUCT)
+            ->orderBy('created_at', 'desc')
+            ->first();
+
         $plans = Plan::where('product', static::PRODUCT)->get();
 
         $productIds = $plans->pluck('plan_id_on_paddle');
-        $prices = app(Products::class)->getProductPrices($productIds, $request->user());
+        $prices = app(Products::class)->getProductPrices($productIds, $request->user(), optional($subscription)->quantity ?? 1);
 
-        $plansCollection = $plans->map(function (Plan $plan) use ($request, $prices): array {
+        $plansCollection = $plans->map(function (Plan $plan) use ($prices, $subscription): array {
             $price = $prices->where('product_id', $plan->plan_id_on_paddle)->first();
 
             return [
@@ -30,27 +46,30 @@ class OfficeLifeController extends Controller
                 'single_price' => $price['price'],
                 'price' => $price['price'],
                 'frequency' => $price['frequency_name'],
-                'quantity' => 1,
-                'url' => [
-                    'pay_link' => $this->getPayLink($request, $plan),
-                ],
+                'quantity' => optional($subscription)->quantity ?? 1,
             ];
         });
 
         $licence = $request->user()->licenceKeys()
-            ->whereIn('plan_id', $plans->pluck('id'))
+            ->where('plan_id', optional(optional($subscription)->plan)->id)
             ->orderBy('created_at', 'desc')
             ->first();
 
         return Inertia::render('OfficeLife/Index', [
-            'data' => [
-                'plans' => $plansCollection,
-                'current_licence' => $licence,
-            ],
+            'plans' => $plansCollection,
+            'current_licence' => $licence,
+            'refresh' => $request->boolean('refresh'),
         ]);
     }
 
-    public function price(OfficeLifePriceRequest $request, Plan $plan)
+    /**
+     * Get the updated price for the given plan.
+     *
+     * @param  \App\Http\Requests\OfficeLifePriceRequest  $request
+     * @param  \App\Models\Plan  $plan
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function price(OfficeLifePriceRequest $request, Plan $plan): JsonResponse
     {
         if ($plan->product !== static::PRODUCT) {
             abort(401);
@@ -65,15 +84,46 @@ class OfficeLifeController extends Controller
 
         return response()->json([
             'price' => $price['price'],
-            'pay_link' => $this->getPayLink($request, $plan, $request->quantity()),
         ]);
     }
 
-    private function getPayLink(Request $request, Plan $plan, int $quantity = 1)
+    /**
+     * Subscribe to this plan.
+     *
+     * @param  \App\Http\Requests\OfficeLifePriceRequest  $request
+     * @param  Plan  $plan
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(OfficeLifePriceRequest $request, Plan $plan)
     {
-        return $request->user()->newSubscription($plan->plan_name, $plan->plan_id_on_paddle)
+        if ($plan->product !== static::PRODUCT) {
+            abort(401);
+        }
+
+        $url = $request->user()->newSubscription($plan->plan_name, $plan->plan_id_on_paddle)
             ->returnTo(route('officelife.index').'?refresh=true')
-            ->quantity($quantity)
+            ->quantity($request->quantity())
             ->create();
+
+        return Inertia::location($url);
+    }
+
+    /**
+     * Update subscription plan.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request): RedirectResponse
+    {
+        app(UpdateSubscription::class)->execute([
+            'user_id' => $request->user()->id,
+            'plan_id' => $request->input('plan_id'),
+            'quantity' => $request->input('quantity'),
+        ]);
+
+        return back()->with([
+            'refresh' => true,
+        ]);
     }
 }
--- tests/Feature/Controllers/MonicaControllerTest.php
+++ tests/Feature/Controllers/MonicaControllerTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature\Controllers;
 use App\Helpers\Products;
 use App\Models\LicenceKey;
 use App\Models\Plan;
+use App\Models\Subscription;
 use App\Models\User;
+use App\Services\UpdateSubscription;
 use Illuminate\Support\Facades\Http;
 use Mockery\MockInterface;
 use Tests\TestCase;
@@ -21,14 +23,6 @@ class MonicaControllerTest extends TestCase
             'plan_id_on_paddle' => 1,
         ]);
 
-        Http::fake([
-            'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link' => Http::response([
-                'success' => true,
-                'response' => [
-                    'url' => 'https://sandbox-vendors.paddle.com/example',
-                ],
-            ], 200),
-        ]);
         $this->mock(Products::class, function (MockInterface $mock) use ($plan) {
             $mock->shouldReceive('getProductPrices')
                 ->andReturn(collect([
@@ -43,18 +37,8 @@ class MonicaControllerTest extends TestCase
 
         $response = $this->actingAs($user)->get('/monica');
 
-        Http::assertSent(function ($request) use ($user) {
-            $this->assertEquals('https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link', $request->url());
-            $this->assertEquals('POST', $request->method());
-            $this->assertStringContainsString('"product_id":"1"', $request->body());
-            $this->assertStringContainsString('\"billable_id\":'.$user->id, $request->body());
-
-            return true;
-        });
-
         $response->assertStatus(200);
         $response->assertSee('MonicaPlan');
-        $response->assertSee('https:\\/\\/sandbox-vendors.paddle.com\\/example');
     }
 
     /** @test */
@@ -70,14 +54,6 @@ class MonicaControllerTest extends TestCase
             'plan_id_on_paddle' => 2,
         ]);
 
-        Http::fake([
-            'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link' => Http::response([
-                'success' => true,
-                'response' => [
-                    'url' => 'https://sandbox-vendors.paddle.com/example',
-                ],
-            ], 200),
-        ]);
         $this->mock(Products::class, function (MockInterface $mock) use ($plan) {
             $mock->shouldReceive('getProductPrices')
                 ->andReturn(collect([
@@ -92,18 +68,8 @@ class MonicaControllerTest extends TestCase
 
         $response = $this->actingAs($user)->get('/monica');
 
-        Http::assertSent(function ($request) use ($user) {
-            $this->assertEquals('https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link', $request->url());
-            $this->assertEquals('POST', $request->method());
-            $this->assertStringContainsString('"product_id":"1"', $request->body());
-            $this->assertStringContainsString('\"billable_id\":'.$user->id, $request->body());
-
-            return true;
-        });
-
         $response->assertStatus(200);
         $response->assertSee('MonicaPlan');
-        $response->assertSee('https:\\/\\/sandbox-vendors.paddle.com\\/example');
         $response->assertDontSee('OfficeLifePlan');
     }
 
@@ -118,15 +84,11 @@ class MonicaControllerTest extends TestCase
             'plan_id' => $plan->id,
             'key' => 'abc123',
         ]);
-
-        Http::fake([
-            'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link' => Http::response([
-                'success' => true,
-                'response' => [
-                    'url' => 'https://sandbox-vendors.paddle.com/example',
-                ],
-            ], 200),
+        Subscription::factory()->create([
+            'billable_id' => $user->id,
+            'paddle_plan' => $plan->plan_id_on_paddle,
         ]);
+
         $this->mock(Products::class, function (MockInterface $mock) use ($plan) {
             $mock->shouldReceive('getProductPrices')
                 ->andReturn(collect([
@@ -165,14 +127,6 @@ class MonicaControllerTest extends TestCase
             'plan_id' => $plan->id,
         ]);
 
-        Http::fake([
-            'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link' => Http::response([
-                'success' => true,
-                'response' => [
-                    'url' => 'https://sandbox-vendors.paddle.com/example',
-                ],
-            ], 200),
-        ]);
         $this->mock(Products::class, function (MockInterface $mock) use ($plan) {
             $mock->shouldReceive('getProductPrices')
                 ->andReturn(collect([
@@ -189,5 +143,64 @@ class MonicaControllerTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertDontSee('abc123');
+    }
+
+    /** @test */
+    public function it_updates_subscription_plan(): void
+    {
+        $user = User::factory()->create();
+        $plan = Plan::factory()->monica()->create();
+
+        $this->mock(UpdateSubscription::class, function (MockInterface $mock) use ($user, $plan) {
+            $mock->shouldReceive('execute')
+                ->once()
+                ->withArgs(function ($data) use ($user, $plan) {
+                    $this->assertEquals([
+                        'user_id' => $user->id,
+                        'plan_id' => $plan->id,
+                    ], $data);
+
+                    return true;
+                });
+        });
+
+        $response = $this->actingAs($user)->patch('/monica', [
+            'plan_id' => $plan->id,
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    /** @test */
+    public function it_subscribes_to_a_plan(): void
+    {
+        $user = User::factory()->create();
+        $plan = Plan::factory()->monica()->create([
+            'friendly_name' => 'MonicaPlan',
+            'plan_id_on_paddle' => 1,
+        ]);
+
+        Http::fake([
+            'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link' => Http::response([
+                'success' => true,
+                'response' => [
+                    'url' => 'https://sandbox-vendors.paddle.com/example',
+                ],
+            ], 200),
+        ]);
+
+        $response = $this->actingAs($user)->post("/monica/{$plan->id}");
+
+        Http::assertSent(function ($request) use ($user) {
+            $this->assertEquals('https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link', $request->url());
+            $this->assertEquals('POST', $request->method());
+            $this->assertStringContainsString('"product_id":1', $request->body());
+            $this->assertStringContainsString('\"billable_id\":'.$user->id, $request->body());
+
+            return true;
+        });
+
+        $response->assertStatus(302);
+        $response->assertRedirect('https://sandbox-vendors.paddle.com/example');
     }
 }
