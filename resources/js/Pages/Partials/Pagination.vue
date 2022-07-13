d293dfe3d7648e201c792fdfcfddf78fe55af914
Alexis Saettler <alexis@saettler.org>
2022-07-14 00:08:36 +0200

    simplification

--- resources/js/Pages/Partials/Pagination.vue
+++ resources/js/Pages/Partials/Pagination.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed } from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
+import ChevronLeft from './ChevronLeft.vue';
+import ChevronRight from './ChevronRight.vue';
 
 const props = defineProps({
   items: Object,
@@ -8,87 +10,81 @@ const props = defineProps({
 
 const firstItem = computed(() => props.items.data.length > 0 ? (props.items.meta.current_page - 1) * props.items.meta.per_page + 1 : null);
 const lastItem = computed(() => props.items.data.length > 0 ? firstItem.value + props.items.data.length - 1 : null);
+
+const commonClasses = 'relative inline-flex items-center px-4 py-2 text-sm font-medium bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 leading-5';
+const linkClasses = 'hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 dark:ring-gray-700 focus:border-blue-300 focus:dark:border-blue-700 active:bg-gray-100 active:dark:bg-gray-900 active:text-gray-700 active:dark:text-gray-300 transition ease-in-out duration-150';
+
 </script>
 
 <template>
   <nav v-if="items.meta.current_page !== 1 || items.meta.last_page > 1" role="navigation" :aria-label="$t('Pagination Navigation')" class="flex items-center justify-between">
     <div class="flex justify-between flex-1 sm:hidden">
-      <span v-if="items.meta.current_page === 1" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 cursor-default leading-5 rounded-md"
+      <span v-if="items.meta.current_page === 1" :class="[commonClasses, 'rounded-md', 'text-gray-500', 'cursor-default']"
         v-html="$t('pagination.previous')">
       </span>
-      <Link v-else :href="items.links.prev" preserve-scroll class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 dark:ring-gray-700 focus:border-blue-300 focus:dark:border-blue-700 active:bg-gray-100 active:dark:bg-gray-900 active:text-gray-700 active:dark:text-gray-300 transition ease-in-out duration-150">
+      <Link v-else :href="items.links.prev" preserve-scroll :class="[commonClasses, linkClasses, 'rounded-md', 'text-gray-700', 'dark:text-gray-300']">
         <span v-html="$t('pagination.previous')"></span>
       </Link>
 
-      <Link v-if="items.meta.last_page > 1" :href="items.links.next" preserve-scroll class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 dark:ring-gray-700 focus:border-blue-300 focus:dark:border-blue-700 active:bg-gray-100 active:dark:bg-gray-900 active:text-gray-700 active:dark:text-gray-300 transition ease-in-out duration-150">
+      <Link v-if="items.meta.last_page > 1" :href="items.links.next" preserve-scroll :class="[commonClasses, linkClasses, 'ml-3', 'rounded-md', 'text-gray-700', 'dark:text-gray-300']">
         <span v-html="$t('pagination.next')"></span>
       </Link>
-      <span v-else class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 cursor-default leading-5 rounded-md"
+      <span v-else :class="[commonClasses, 'ml-3', 'rounded-md', 'text-gray-500', 'cursor-default']"
         v-html="$t('pagination.next')">
       </span>
     </div>
 
-    <p class="hidden text-sm w-full text-gray-700 dark:text-gray-300 leading-5">
-      <span v-if="firstItem">
-        {{
-          $t("Showing :first to :last of :total results", {
-            first: firstItem,
-            last: lastItem,
-            total: items.meta.total,
-          })
-        }}
-      </span>
-      <span v-else>
-        {{
-          $t("Showing :count of :total results", {
-            count: items.data.length,
-            total: items.meta.total,
-          })
-        }}
-      </span>
-    </p>
-
-    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-      <div class="relative z-0 inline-flex shadow-sm rounded-md">
-        <span v-if="items.meta.current_page === 1" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 cursor-default rounded-l-md leading-5"
-          aria-hidden="true" aria-disabled="true" :aria-label="$t('pagination.previous')">
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-          </svg>
+    <div class="hidden sm:flex-1 sm:flex sm:flex-col sm:items-center sm:justify-between">
+      <p class="text-sm text-gray-700 dark:text-gray-300 leading-5">
+        <span v-if="firstItem">
+          {{
+            $t('Showing :first to :last of :total results', {
+              first: firstItem,
+              last: lastItem,
+              total: items.meta.total,
+            })
+          }}
         </span>
-        <Link v-else :href="items.links.prev" preserve-scroll rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-l-md leading-5 hover:text-gray-400 hover:dark:text-gray-600 focus:z-10 focus:outline-none focus:ring ring-gray-300 dark:ring-gray-700 focus:border-blue-300 focus:dark:border-blue-700 active:bg-gray-100 active:dark:bg-gray-900 active:text-gray-500 transition ease-in-out duration-150"
+        <span v-else>
+          {{
+            $t('Showing :count of :total results', {
+              count: items.data.length,
+              total: items.meta.total,
+            })
+          }}
+        </span>
+      </p>
+
+      <div class="relative z-0 inline-flex shadow-sm rounded-md">
+        <span v-if="items.meta.current_page === 1" :class="[commonClasses, 'px-2', 'rounded-l-md', 'text-gray-500', 'focus:z-10', 'cursor-default']"
+          aria-hidden="true" aria-disabled="true" :aria-label="$t('pagination.previous')">
+          <ChevronLeft />
+        </span>
+        <Link v-else :href="items.links.prev" preserve-scroll rel="prev" :class="[commonClasses, linkClasses, 'px-2', 'rounded-l-md', 'text-gray-500', 'hover:text-gray-400', 'hover:dark:text-gray-600', 'focus:z-10']"
           :aria-label="$t('pagination.previous')">
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-          </svg>
+          <ChevronLeft />
         </Link>
 
         <template v-for="(link, id) in items.meta.links" :key="id">
-          <span v-if="link.url === null" aria-disabled="true"
-            class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 dark:text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 cursor-default leading-5"
+          <span v-if="link.url === null" aria-disabled="true" :class="[commonClasses, '-ml-px', 'text-gray-700', 'dark:text-gray-500', 'cursor-default']"
             v-html="link.label">
           </span>
-          <span v-else-if="link.active" aria-current="page" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-500 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 cursor-default leading-5"
+          <span v-else-if="link.active" aria-current="page" :class="[commonClasses, '-ml-px', 'text-gray-500', 'bg-gray-100', 'dark:bg-gray-800', 'cursor-default']"
             v-html="link.label">
           </span>
-          <Link v-else :href="link.url" preserve-scroll class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 dark:ring-gray-700 focus:border-blue-300 focus:dark:border-blue-700 active:bg-gray-100 active:dark:bg-gray-900 active:text-gray-700 active:dark:text-gray-300 transition ease-in-out duration-150"
-            :aria-label="$t('Go to page :page', { page: link.label })">
+          <Link v-else :href="link.url" preserve-scroll :class="[commonClasses, linkClasses, '-ml-px', 'text-gray-700', 'dark:text-gray-300', 'focus:z-10']" :aria-label="$t('Go to page :page', { page: link.label })">
             <span v-html="link.label"></span>
           </Link>
         </template>
 
         <Link v-if="items.meta.current_page < items.meta.last_page"
-          :href="items.links.next" preserve-scroll rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 dark:ring-gray-700 focus:border-blue-300 focus:dark:border-blue-700 active:bg-gray-100 active:dark:bg-gray-900 active:text-gray-500 transition ease-in-out duration-150"
+          :href="items.links.next" preserve-scroll rel="next" :class="[commonClasses, linkClasses, '-ml-px', 'px-2', 'rounded-r-md', 'text-gray-500', 'hover:text-gray-400', 'hover:dark:text-gray-600', 'focus:z-10']"
           :aria-label="$t('pagination.next')">
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-          </svg>
+          <ChevronRight />
         </Link>
-        <span v-else class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 cursor-default rounded-r-md leading-5"
+        <span v-else :class="[commonClasses, linkClasses, '-ml-px', 'px-2', 'rounded-r-md', 'text-gray-500', 'focus:z-10', 'cursor-default']"
           aria-hidden="true" aria-disabled="true" :aria-label="$t('pagination.next')">
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-          </svg>
+          <ChevronRight />
         </span>
       </div>
     </div>
