bfd8fc5159d77ad322138bbbb7bb98502674a2f5
Alexis Saettler <alexis@saettler.org>
2022-07-13 08:12:05 +0200

    add caption

--- resources/js/Pages/Administration/Index.vue
+++ resources/js/Pages/Administration/Index.vue
@@ -84,6 +84,9 @@ defineProps({
         <!-- table -->
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <caption class="hidden">
+              Users
+            </caption>
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-300">
               <tr>
                 <th scope="col" class="px-6 py-3">
