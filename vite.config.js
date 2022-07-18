1bcb89e7af887e25a8d991e10196a66970e43b66
Alexis Saettler <alexis@saettler.org>
2022-07-18 23:39:56 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import _ from 'lodash';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
