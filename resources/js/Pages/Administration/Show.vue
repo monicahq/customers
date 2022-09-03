874053034732d8cb0e266bb8b6f0d9b9dfa2f3d1
Alexis Saettler <alexis@saettler.org>
2022-09-03 12:45:23 +0200

    style: fix dark style

--- resources/js/Pages/Administration/Show.vue
+++ resources/js/Pages/Administration/Show.vue
@@ -28,7 +28,7 @@ const officeLifeLicences = computed(() => _.filter(props.licences, (licence) => 
             <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
             </svg>
-            <Link :href="route('administration.index')" class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-100 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+            <Link :href="route('administration.index')" class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-100 md:ml-2 dark:text-gray-400 hover:dark:text-white">
               Users
             </Link>
           </div>
